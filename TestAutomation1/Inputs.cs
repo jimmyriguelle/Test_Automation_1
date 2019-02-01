b12ecbde39d5ce5c35da9b3a7acb0a5001a76c03
Jimmy Riguelle <jimmy.riguelle@sogeti.com>
2019-02-01 20:56:20 +0100

    Install Selenium webDriver

---
+++ TestAutomation1/Inputs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAutomation1
+{
+    [TestClass]
+    public class Inputs
+    {
+        [TestMethod]
+        public void textBoxes()
+        {
+            IWebDriver
+        }
+    }
+}
