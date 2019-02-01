661ed17169bf6aa197e09d7c336c177fa53884ec
Jimmy Riguelle <jimmy.riguelle@sogeti.com>
2019-02-01 21:12:20 +0100

    Add some methods

--- TestAutomation1/Inputs.cs
+++ TestAutomation1/Inputs.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TestAutomation1
 {
@@ -9,7 +11,28 @@ namespace TestAutomation1
         [TestMethod]
         public void textBoxes()
         {
-            IWebDriver
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://formy-project.herokuapp.com/form");
+
+            IWebElement text = driver.FindElement(By.Id("first-name"));
+
+            IWebElement radioButton = driver.FindElement(By.Id("radio-button-1"));
+            radioButton.Click();
+
+            text.Clear();
+            text.SendKeys("sending text");
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        //
+        [TestMethod]
+        public void radioButton()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://formy-project.herokuapp.com/form");
+
+            IWebElement radioButton = driver.FindElement(By.Id("radio-button-1"));
+            radioButton.Click();
         }
     }
 }
